5c71fc588ae6f1c4f0931409b9d05e2bb1d20edf
ryanlow-rp <51071982+ryanlow-rp@users.noreply.github.com>
2023-04-26 16:00:31 +0800

    Update MainActivity.java

--- app/src/main/java/sg/edu/rp/c346/problem02/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/problem02/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        uncomment as necessary
 //        setContentView(R.layout.activity_main1);
 //        setContentView(R.layout.activity_main2);
 //        setContentView(R.layout.activity_main3);
